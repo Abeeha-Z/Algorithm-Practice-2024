1972e1a7097e6b13eddc374037dd8743de5bb2ac
Abeeha-Z <158211569+Abeeha-Z@users.noreply.github.com>
2024-10-21 13:57:56 -0400

    4t

---
+++ notes.js
@@ -0,0 +1,19 @@
+//factorials my code
+function factorial(n) {
+    for(i = 1; i <= n; i++) {
+        x = i*(i+1); 
+        console.log(x)
+    }
+}
+
+factorial(10);
+
+//Answer Shown
+function factorial(n) {
+let result = 0
+for(let i =1; i<=n; i++) {
+    result *= i;
+}
+return result
+}
+console.log(factorial(10))
