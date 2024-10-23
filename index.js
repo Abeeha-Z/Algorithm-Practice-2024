66ac2485ed5efbc02313d1bb9510a2d779f1e06f
Abeeha-Z <158211569+Abeeha-Z@users.noreply.github.com>
2024-10-23 13:58:41 -0400

    tghtgh

--- index.js
+++ index.js
@@ -18,3 +18,15 @@ return result
 }
 console.log(factorial(10))
 
+//Parking Spaces Problem
+function checkSpaces(n, yesterday, today) {
+    for (let i = 0; i < n, i++) {
+        if (yesterday[i] === 'C' && today[i] === 'C') {
+            count++;
+        }
+    }
+    return count;
+}
+
+console.long(checkSpaces(5, CC..C, "C..C."));
+
