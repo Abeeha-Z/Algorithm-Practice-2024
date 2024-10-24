e1c3890da8837d3ce84f0105c80a7a648f10cbe7
Abeeha-Z <158211569+Abeeha-Z@users.noreply.github.com>
2024-10-24 13:58:08 -0400

    hh

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ console.log(factorial(10))
 
 //Parking Spaces Problem
 function checkSpaces(n, yesterday, today) {
-    for (let i = 0; i < n, i++) {
+    for (let i = 0; i < n; i++) {
         if (yesterday[i] === 'C' && today[i] === 'C') {
             count++;
         }
@@ -28,5 +28,21 @@ function checkSpaces(n, yesterday, today) {
     return count;
 }
 
-console.long(checkSpaces(5, CC..C, "C..C."));
+console.long(checkSpaces(5, "CC..C", "C..C."));
 
+//Notes: Binary and Hexadecimal
+//Read right to left, multiply digit by 2 or 16 to the __ power, starting from 0 up to 7 for binary and 15 for binary
+//Hexa: 1A3 - 3 x 16^0 = 3, A = 10 x 16^1 = 160, 1 x 16^2 = 256, total = 419
+//Binary: 1 and 0 represent whether somehting exists or doesn't, zero means a blank space, move to the next power of 2
+//1101 = 13, because 1 x 2^0 = 1, 0 x 2^1 = 0, 1 x 2^2 = 4, 1 x 2^3 = 8, total = 13
+
+//Convert Binary to Base ten
+function convert(value) {
+    for(let i = 0; i <= 7; i++) {
+        x = value[0]
+        if (x == '1') {
+            x*2**i
+        }
+        
+    }
+}
